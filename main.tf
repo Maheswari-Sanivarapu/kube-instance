62ffd4589347d2e38ab10fb893015a20413137fb
maheswari <mahi.sgs22@gmail.com>
2025-07-27 09:09:16 +0530

    kube

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "kube_instance" {
     ami = local.ami_id
-    vpc_security_group_ids = [aws_security_group.allow_all_k8s]
+    vpc_security_group_ids = [aws_security_group.allow_all_k8s.id]
     instance_type = "t3.micro"
     user_data = file("k8s.sh")
     tags = {
